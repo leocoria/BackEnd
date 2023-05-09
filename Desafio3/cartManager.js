bc31bc5e317f1a36b0062e9aa71fd8a9d81aa57b
Leo Coria <leo.coria92@gmail.com>
2023-05-09 16:31:50 -0300

    Modifico CartManager

--- Desafio3/cartManager.js
+++ Desafio3/cartManager.js
@@ -8,9 +8,28 @@ export default class cartManager {
       fs.writeFileSync(this.path, JSON.stringify([]));
     }
   }
-  async getCartProducts(cid) {}
 
-  async addCart() {}
+  async #getCarts() {
+    try {
+      const actualCarts = await fs.promises.readFile(this.path, "utf-8");
+      return JSON.parse(actualCarts);
+    } catch (err) {
+      console.log("No puedo darte los productos", err);
+    }
+  }
+
+  async getCartProducts(cid) {
+    const carts = await this.#getCarts();
+    const found = carts.find((cart) => cart.id === cid);
+    const notFound = "No se encuentra el producto";
+    if (found) {
+      return found;
+    } else {
+      return notFound;
+    }
+  }
+
+  async addCart(products) {}
 
   async addProductToCart(cid, pid) {}
 
